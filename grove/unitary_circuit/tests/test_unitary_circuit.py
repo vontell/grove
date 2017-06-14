7425d4d9b08d671f42c94f7a2953d464e3a2e6ef
Joseph Lin <joelin2014@gmail.com>
2017-06-14 15:27:39 -0700

    Modify tests so they do not run large qubit operations.

--- grove/unitary_circuit/tests/test_unitary_circuit.py
+++ grove/unitary_circuit/tests/test_unitary_circuit.py
@@ -19,7 +19,7 @@ def try_and_or():
     result = cxn.run_and_measure(p, [q.index() for q in [q1, q2, sb]])
     print result
 
-if __name__=='__main__':
+def test_two_bit_to_one_bit():
     p = pq.Program()
     q1 = p.alloc()
     q2 = p.alloc()
@@ -27,9 +27,35 @@ if __name__=='__main__':
 
     f = [0, 1, 1, 0]
     num_range_bits = 1
-    outbits = add_function(p, lambda x: f[x], [q2, q1], 1)
+    outbits = add_function(p, lambda x: f[x], [q2, q1], num_range_bits)
     print p.out()
+    print len(p.get_qubits())
 
     cxn = Connection()
     result = cxn.run_and_measure(p, [q.index() for q in outbits])
     print result
+
+def test_three_to_three():
+    """
+    Currently doesn't run cuz too many qubits
+    :return:
+    """
+    p = pq.Program()
+    q1 = p.alloc()
+    q2 = p.alloc()
+    q3 = p.alloc()
+    p.inst(X(q2))
+    p.inst(X(q1))
+
+    f = [0, 1, 2, 3, 4, 5, 6, 7]
+    num_range_bits = 3
+    outbits = add_function(p, lambda x: f[x], [q3, q2, q1], num_range_bits)
+    print p.out()
+    print len(p.get_qubits())
+
+    # cxn = Connection()
+    # result = cxn.run_and_measure(p, [q.index() for q in outbits])
+    # print result
+
+if __name__=='__main__':
+    test_two_bit_to_one_bit()
