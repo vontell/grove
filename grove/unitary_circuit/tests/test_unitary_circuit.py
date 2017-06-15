722d80fef3489969962fc535a1b02bf6c8c91284
Joseph Lin <joelin2014@gmail.com>
2017-06-15 16:59:57 -0700

    Added function and test that successfully implements reversible function using gates

--- grove/unitary_circuit/tests/test_unitary_circuit.py
+++ grove/unitary_circuit/tests/test_unitary_circuit.py
@@ -23,9 +23,9 @@ def test_two_bit_to_one_bit():
     p = pq.Program()
     q1 = p.alloc()
     q2 = p.alloc()
-    p.inst(X(q2))
+    p.inst(X(q1))
 
-    f = [0, 1, 1, 0]
+    f = [1, 1, 1, 1]
     num_range_bits = 1
     outbits = add_function(p, lambda x: f[x], [q2, q1], num_range_bits)
     print p.out()
@@ -57,5 +57,113 @@ def test_three_to_three():
     # result = cxn.run_and_measure(p, [q.index() for q in outbits])
     # print result
 
+def test_and_all():
+    p = pq.Program()
+    q1 = p.alloc()
+    q2 = p.alloc()
+    q3 = p.alloc()
+    p.inst(X(q3))
+    p.inst(X(q2))
+    p.inst(X(q1))
+
+    p, sb = and_all(p, [q3, q2, q1])
+
+    print p.out()
+
+    cxn = Connection()
+    result = cxn.run_and_measure(p, [sb.index()])
+
+    print p.get_qubits()
+    print result
+
+def test_or_all():
+    p = pq.Program()
+    q1 = p.alloc()
+    q2 = p.alloc()
+    q3 = p.alloc()
+    p.inst(X(q2))
+
+    sb = or_all(p, [q3, q2, q1])
+
+    print p.out()
+    print p.get_qubits()
+
+    cxn = Connection()
+    result = cxn.run_and_measure(p, [sb.index()])
+
+    print result
+
+# Using the basic algorithm found here: https://s2.smu.edu/~mitch/class/8381/papers/MMD-DAC03.pdf
+def test_add_reversible_function(x):
+    p = pq.Program()
+
+    q1 = p.alloc()
+    q2 = p.alloc()
+    q3 = p.alloc()
+    qubits = [q3, q2, q1]
+    num_qubits = len(qubits)
+
+    n = x
+    for i in xrange(num_qubits):
+        if n % 2 == 1:
+            p.inst(X(qubits[num_qubits - 1 - i]))
+        n /= 2
+
+    mappings = [1, 0, 3, 2, 5, 7, 4, 6]
+    f = lambda j: mappings[j]
+    add_reversible_function(p, f, qubits)
+    p.out()
+
+    cxn = Connection()
+    result = cxn.run_and_measure(p, [q.index() for q in qubits])
+
+    y = reduce(lambda prev, next: (prev << 1) + next, result[0], 0)
+    print "-"*20
+    print "Test for x = ", x
+    print "Expected: ", f(x)
+    print "Got: ", y
+    print "-"*20
+
+    assert f(x) == y
+
+def test_four_bit_add_reversible_function(x):
+    p = pq.Program()
+
+    q1 = p.alloc()
+    q2 = p.alloc()
+    q3 = p.alloc()
+    q4 = p.alloc()
+    qubits = [q4, q3, q2, q1]
+    num_qubits = len(qubits)
+
+    n = x
+    for i in xrange(num_qubits):
+        if n % 2 == 1:
+            p.inst(X(qubits[num_qubits - 1 - i]))
+        n /= 2
+
+    mappings = [13, 9, 7, 6,
+                1, 0, 12, 5,
+                4, 8, 14, 3,
+                2, 11, 10, 15]
+
+    f = lambda j: mappings[j]
+    add_reversible_function(p, f, qubits)
+    p.out()
+
+    cxn = Connection()
+    result = cxn.run_and_measure(p, [q.index() for q in qubits])
+
+    y = reduce(lambda prev, next: (prev << 1) + next, result[0], 0)
+    print "-"*20
+    print "Test for x = ", x
+    print "Expected: ", f(x)
+    print "Got: ", y
+    print "-"*20
+
+    assert f(x) == y
+
+
 if __name__=='__main__':
-    test_two_bit_to_one_bit()
+    for x in range(2**4):
+        test_four_bit_add_reversible_function(x)
